7ffc7d713c2a44c80413604b0a5bab2685b80493
Sarah <sarah.btn.verdom@gmail.com>
2024-03-22 10:53:17 +0100

    Ajoutez des fichiers projet.

---
+++ GestBanque/Program.cs
@@ -0,0 +1,30 @@
+﻿using Models;
+namespace GestBanque;
+
+class program
+{
+    static void main(string[]args)
+    {
+        Personne doeJohn = new Personne()
+        {
+            Nom = "Doe",
+            Prenom = "John",
+            DateNaiss = new DateTime(2000, 02, 29)
+        };
+
+        Courant courant = new Models.Courant()
+        {
+            Numero = "0001",
+            LigneDeCredit = 500,
+            Titulaire = doeJohn
+        };
+
+        courant.Depot(-100);
+        courant.Depot(100);
+        courant.Retrait(-100);
+        courant.Retrait(100);
+        courant.Retrait(600);
+
+        Console.WriteLine(courant.Solde);
+    }
+}
